8a8c2600ab832affdc0ab557ea6066b1698b2b33
Zeeshan Safdar <safdarzeeshan@gmail.com>
2016-04-20 21:15:59 -0400

    added config file

--- app.rb
+++ app.rb
@@ -6,68 +6,63 @@ require 'json'
 require 'ingreedy'
 require 'sinatra/cross_origin'
 
-configure do
-  enable :cross_origin
-end
+class RecipeScraper < Sinatra::Base
 
-get '/' do
+    configure do
+      enable :cross_origin
+    end
 
-    recipe_url = params['url']
-    recipe_html_string = open(recipe_url).read
-    puts 'recipe url read'
-    recipe = Hangry.parse(recipe_html_string)
+    get '/' do
 
-    puts recipe.canonical_url
-    # puts recipe.to_h.to_json.fetch('author')
+        recipe_url = params['url']
+        recipe_html_string = open(recipe_url).read
+        puts 'recipe url read'
+        recipe = Hangry.parse(recipe_html_string)
 
+        puts recipe.canonical_url
 
-    # url give no information
+        if recipe.canonical_url == nil
+            puts 'recipe url cannot be parsed'
+            status 400
+            content_type :json
+            { :recipe_url => recipe_url, :error => 'Unable to get recipe information form this URL.'}.to_json
 
-    # if recipe.canonical_url == nil
-    #     puts 'recipe url cannot be parsed'
-    #     content_type :json
-    #     { :recipe_url => recipe_url, :error => 'no information available'}.to_json
+        # recipe blog can be parsed
+        else
 
-    if recipe.canonical_url == nil
-        puts 'recipe url cannot be parsed'
-        status 400
-        content_type :json
-        { :recipe_url => recipe_url, :error => 'Unable to get recipe information form this URL.'}.to_json
+            ingedient_list = Array.new
 
-    # recipe blog can be parsed
-    else
+            for i in recipe.ingredients
+                # ingedient_n = Array.new
+                begin
+                ingredient = Ingreedy.parse(i)
+                rescue
+                    puts 'error ' + i.to_s + ' cannot be parsed'
+                    # {ingedient_n.push(}:name => i)
+                    ingedient_list.push({:ingredient => {:name => i}, :amount => '' })
+                    next
+                end
 
-        ingedient_list = Array.new
-
-        for i in recipe.ingredients
-            # ingedient_n = Array.new
-            begin
-            ingredient = Ingreedy.parse(i)
-            rescue
-                puts 'error ' + i.to_s + ' cannot be parsed'
-                # {ingedient_n.push(}:name => i)
-                ingedient_list.push({:ingredient => {:name => i}, :amount => '' })
-                next
+                ingedient_list.push({:ingredient => {:name => ingredient.ingredient}, :amount => to_frac(ingredient.amount.to_s).to_s + ' ' + ingredient.unit.to_s})
             end
 
-            ingedient_list.push({:ingredient => {:name => ingredient.ingredient}, :amount => to_frac(ingredient.amount.to_s).to_s + ' ' + ingredient.unit.to_s})
+            content_type :json
+            { :url => recipe.canonical_url, :name => recipe.name, :time => recipe.cook_time, :snippet => recipe.description, :image_url => recipe.image_url, :serving_size => recipe.yield, :ingredients => ingedient_list}.to_json
+
+        end
+    end
+
+    def to_frac(value)
+        numerator, denominator = value.split('/').map(&:to_f)
+
+        if denominator !=1
+            return value
         end
 
-        content_type :json
-        { :url => recipe.canonical_url, :name => recipe.name, :time => recipe.cook_time, :snippet => recipe.description, :image_url => recipe.image_url, :serving_size => recipe.yield, :ingredients => ingedient_list}.to_json
+        if denominator ==1
+            return numerator.to_i
+        end
 
-    end
-end
-
-def to_frac(value)
-    numerator, denominator = value.split('/').map(&:to_f)
-
-    if denominator !=1
-        return value
-    end
-
-    if denominator ==1
-        return numerator.to_i
     end
 
 end
