615eec9d7c65b9398ab2e313d6738e4d850a6d4c
Zeeshan Safdar <safdarzeeshan@gmail.com>
2016-10-03 11:19:51 -0400

    check image resolution option

--- app.rb
+++ app.rb
@@ -10,6 +10,7 @@ require 'json'
 require 'ingreedy'
 require 'sinatra/cross_origin'
 require 'nokogiri'
+require 'fastimage'
 
 
 configure do
@@ -22,8 +23,6 @@ get '/' do
     recipe_html_string = open(recipe_url).read
     puts 'recipe url read'
     recipe = Hangry.parse(recipe_html_string)
-
-    puts recipe
 
     #error if recipe blog url cannot be parsed
     if recipe.canonical_url == nil
@@ -44,7 +43,6 @@ get '/' do
             ingredient = Ingreedy.parse(i)
             rescue
                 puts 'error ' + i.to_s + ' cannot be parsed'
-                # {ingedient_n.push(}:name => i)
                 ingedient_list.push({:ingredient => {:name => i}, :amount => '' })
                 next
             end
@@ -52,25 +50,16 @@ get '/' do
             ingedient_list.push({:ingredient => {:name => ingredient.ingredient}, :amount => to_frac(ingredient.amount.to_s).to_s + ' ' + ingredient.unit.to_s})
         end
 
-        if recipe.image_url == nil
-            #get wp image form html
-            puts 'image url not found'
-            page = Nokogiri::HTML(open(recipe_url))
-
-            if page.at_css('img.wp-post-image')
-                image_url = page.css('img.wp-post-image')[0]['src']
-
-            elsif page.at_css("link[itemprop=image]")
-                image_url = page.css('link[itemprop=image]')[0]['href']
-                print image_url
-
-            else
-                image_url = nil
+        # check recipe image_url resolution
+        if recipe.image_url != nil
+            image_size =  FastImage.size(recipe.image_url, :http_header => {'User-Agent' => 'Fake Browser'})
+            puts image_size
+            if (image_size[0] < 400) || (image_size[1] < 400)
+                recipe.image_url = get_new_image(recipe_url)
             end
-            recipe.image_url = image_url
 
-        else
-            puts 'image url found'
+        elsif recipe.image_url == nil
+            recipe.image_url = get_new_image(recipe_url)
         end
 
         content_type :json
@@ -92,12 +81,25 @@ def to_frac(value)
 
 end
 
+def get_new_image (recipe_url)
+    page = Nokogiri::HTML(open(recipe_url))
+    image_urls = page.search('img').map{ |img| img['src'] }
+
+    for image_url in image_urls
+        #check resolution
+        image_type = FastImage.type(image_url)
+        image_size =  FastImage.size(image_url, :http_header => {'User-Agent' => 'Fake Browser'})
+        if (image_type != 'gif') && (image_size[0] > 400) && (image_size[1] > 400)
+            return image_url
+        end
+    end
+
+    return nil
+end
+
 options "*" do
   response.headers["Allow"] = "HEAD,GET,PUT,POST,DELETE,OPTIONS"
   response.headers["Access-Control-Allow-Headers"] = "X-Requested-With, X-HTTP-Method-Override, Content-Type, Cache-Control, Accept,  x-csrftoken, authorization"
-  # response.headers["Access-Control-Allow-Credentials"] = 'true'
-  # response.headers["Access-Control-Allow-Origin"] = '*'
-
   200
 end
 
